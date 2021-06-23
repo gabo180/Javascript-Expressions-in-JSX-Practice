6b1106bb6ffa4c701dbac248ed0baf674f2c0ec5
gabo180 <82070148+gabo180@users.noreply.github.com>
2021-06-23 15:09:17 -0500

    Completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const yourName = "Gabriel Hernandez";
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {yourName}</p>
+    <p>Copyright {currentYear}</p>
+  </div>,
+  document.getElementById("root")
+);
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
-//The paragraphs should say:
+//The paragrapahs should say:
 //Created by YOURNAME.
 //Copyright CURRENTYEAR.
 //E.g.
-//Created by Angela Yu.
+//Created by Angela YU.
 //Copyright 2019.
